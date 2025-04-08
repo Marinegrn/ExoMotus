a22987d68cdf793e67fbf7330f71415d34b20457
Marinegrn <marinegarein@outlook.fr>
2025-04-08 10:02:07 +0200

    review

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function tryWord(word, base) {
     
     return { wellPlaced: wellPlaced, missplaced: missplaced, notInWord: notInWord }
   }
-}
+};
 
 function guess() {
     let base = 'dictionnaire';
